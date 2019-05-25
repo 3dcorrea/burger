ab7980182fad8f07c77a2fd2f1897afb24d12b76
Dan Correa <dancorrea3@gmail.com>
2019-05-24 22:47:45 -0400

    changed name of table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 USE burgers_db;
-INSERT INTO burgers_db
+INSERT INTO burgers
     ("id", "burger_name", "devoured")
 VALUES
     ("?", "bacownator", "false");
-INSERT INTO burgers_db
+INSERT INTO burgers
     ("id", "burger_name", "devoured")
 VALUES
     ("?", "Girth", "false");
-INSERT INTO burgers_db
+INSERT INTO burgers
     ("id", "burger_name", "devoured")
 VALUES
     ("?", "The Possible Burger", "false");
